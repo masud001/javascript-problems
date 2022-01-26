d23c8e8ac85269f860ee6500828b60bd937d4b17
masud001 <madhnagar@gmail.com>
2022-01-26 23:06:16 +0600

    promiss function

--- javaScript-topics/index.js
+++ javaScript-topics/index.js
@@ -1,1 +1,10 @@
-console.log('Hello javaScript Topics!! 🤴');
+const processOrder = () => {
+	console.log('Processing Order for customar 1');
+	let currentTime = new Date().getTime();
+	while (currentTime + 3000 >= new Date().getTime());
+
+	console.log('Processed Order for customar 1');
+};
+console.log('take Order for customar 1');
+processOrder();
+console.log('complete Order for customar 1');
