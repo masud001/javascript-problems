e4f3c4a7501e1c6cee6abe2dbeac0d30f212fb0c
masud001 <madhnagar@gmail.com>
2022-02-02 10:51:34 +0600

    remove extra space

--- javaScript-topics/index.js
+++ javaScript-topics/index.js
@@ -2,7 +2,6 @@ const processOrder = () => {
 	console.log('Processing Order for customar 1');
 	let currentTime = new Date().getTime();
 	while (currentTime + 3000 >= new Date().getTime());
-
 	console.log('Processed Order for customar 1');
 };
 console.log('take Order for customar 1');
