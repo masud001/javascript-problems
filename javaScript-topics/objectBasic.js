ac774ef069e9995ec52b729ae3dabaf1c1bc1101
masud001 <madhnagar@gmail.com>
2023-06-22 01:49:02 +0600

    learn object in javascript

---
+++ javaScript-topics/objectBasic.js
@@ -0,0 +1,13 @@
+const person = {
+	name: ['masud', 'muhamamd'],
+	age: 32,
+	bio: function () {
+		console.log(`${this.name[0]} and ${this.name[1]} is ${this.age} years old`);
+	},
+	introduceSelf: function () {
+		console.log(`Hi! I'm ${this.name[0]}.`);
+	},
+};
+console.log(person);
+person.bio();
+person.introduceSelf();
