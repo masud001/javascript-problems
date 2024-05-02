b372eb8ed88313d758cc00e7e9e2a35d108ea666
masud001 <madhnagar@gmail.com>
2024-05-03 01:09:25 +0600

    update closure

--- javaScript-topics/closure.js
+++ javaScript-topics/closure.js
@@ -1,5 +1,7 @@
 /** @format */
 
+const { functions } = require('lodash');
+
 function a() {
 	let x = 1;
 	return (b) => {
@@ -8,5 +10,56 @@ function a() {
 }
 
 // console.log(a()(10));
-let c = a();
-console.log(c());
+// let c = a();
+// console.log(c());
+
+const functionCreator = () => {
+	var counter = 3;
+	const add3 = (num) => {
+		counter += num;
+		return counter;
+	};
+	return add3;
+};
+const functionGenerator = functionCreator();
+
+const result = functionGenerator(2);
+console.log('🚀 ~ result:', result);
+console.log('🚀 ~ functionGenerator:', functionGenerator);
+
+const outer = () => {
+	let outerVar = 10;
+	const inner = () => {
+		outerVar++;
+	};
+	return inner;
+};
+const result1 = outer();
+console.log('🚀 ~ result1:', result1());
+
+// lazy evaluation and Eager evaluation
+
+function repeater(count) {
+	var str = ''.padStart(count, 'ABC');
+	return function allTheAs() {
+		return str;
+	};
+}
+var A = repeater(10);
+console.log(A());
+
+var merge = function (nums1, m, nums2, n) {
+	let i = m - 1;
+	let j = n - 1;
+	let k = m + n - 1;
+
+	while (j >= 0) {
+		if (i >= 0 && nums1[i] > nums2[j]) {
+			nums1[k--] = nums1[i--];
+		} else {
+			nums1[k--] = nums2[j--];
+		}
+	}
+};
+
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
---
+++ javaScript-topics/decorator.js
@@ -0,0 +1,45 @@
+/** @format */
+
+const oncify = (fn) => {
+	let called = false;
+	const inner = (input) => {
+		if (!called) {
+			const output = fn(input);
+			called = true;
+			return output;
+		}
+		return `I have already been called`;
+	};
+	return inner;
+};
+const add = (num) => num + 10;
+
+const oncifiedAdd = oncify(add);
+console.log(oncifiedAdd(5));
+
+// generator function to get the next value
+function* getNextId() {
+	console.log('1. code before the first yield');
+	yield 100;
+	console.log('2. code before the second yield');
+	yield 200;
+}
+
+const generator = getNextId();
+console.log('🚀 ~ generator:', generator);
+
+console.log(generator.next());
+console.log(generator.next());
+
+function spreadArgs(fn) {
+	return function spread(args) {
+		return fn(...args);
+	};
+}
+
+function f(x, y, z, w) {
+	return x + y + z + w;
+}
+
+var g = spreadArgs(f);
+console.log(g([1, 2, 3, 4, 5, 6]));
